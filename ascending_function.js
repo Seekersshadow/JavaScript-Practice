7f8ce7e03ce4275ca20489f273562f4277392c94
Seekersshadow <rohito11007@gmail.com>
2024-12-01 20:44:05 +0530

    Question 1 Upload

--- ascending_function.js
+++ ascending_function.js
@@ -1,7 +1,14 @@
+/**
+ * Question: 
+ * Write a JavaScript function `isAscending(a, b, c)` that checks whether three numbers `a`, `b`, and `c` 
+ * are in non-decreasing order (ascending order or equal). If all three numbers are equal, log a message 
+ * `"The Numbers are equal"` to the console. The function should return `true` if the numbers are in 
+ * non-decreasing order and `false` otherwise. Test the function with `a = -1, b = -1, c = -1`.
+ */
+
 function isAscending(a, b, c) {
     if (a == b && b == c) {
         console.log("The Numbers are equal")
-
     }
     else if (a <= b && b <= c) {
         return true
